3c4f7ec3f3be28f8b42004935df08f2dd495d059
ratul <ratul0947@gmail.com>
2023-01-17 20:58:51 +0600

    1st

---
+++ src/app.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getSomething(): string {
+    return 'Something 1';
+  }
+
+
+}
