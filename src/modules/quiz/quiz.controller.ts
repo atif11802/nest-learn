38f979093e96cccb4f6bc371e3401be0d6a6143e
ratul <ratul0947@gmail.com>
2023-01-28 01:16:17 +0600

    3rd tuto

--- src/modules/quiz/quiz.controller.ts
+++ src/modules/quiz/quiz.controller.ts
@@ -1,5 +1,6 @@
-import { Controller,Get } from '@nestjs/common';
+import { Controller,Get,Body,Post, HttpCode, UsePipes, ValidationPipe } from '@nestjs/common';
 import { QuizService } from './quiz.service';
+import { CreateQuizDto } from './dto/createQuiz.dto';
 
 @Controller('quiz')
 export class QuizController {
@@ -15,4 +16,15 @@ export class QuizController {
   getHello(): string {
     return this.quizService.getHello();
   }
+
+  @Post("/create")
+
+  @HttpCode(200)
+  @UsePipes(new ValidationPipe())
+  createQuiz(@Body() quiz  : CreateQuizDto) {
+    return {
+      data : quiz
+    }
+  }
+
 }
